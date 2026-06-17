bca41fc46183f02c595f80e97002213f333a8090
magysil <magallysilvab@gmail.com>
2026-06-16 21:13:51 -0400

    fix: remuevo tipos TS y ajusto connectionString

--- db.js
+++ db.js
@@ -1,25 +1,11 @@
 import "dotenv/config";
-import pkg from "pg";
+import pg from 'pg';
 
-const { Pool } = pkg;
-
-/* export const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-}); */
-
-// Render proporciona una URL completa, por lo que usamos connectionString
-export const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false, // Obligatorio para que Render acepte la conexión segura
-    },
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-// Opcional: Un log para confirmar que la conexión está intentando establecerse
-pool.on('connect', () => {
-    console.log('Conectado a la base de datos PostgreSQL en Render');
-});
+export default pool;
